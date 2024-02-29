a06cb86c1e5797df655137fe7fa43fa15911d704
lvo711717 <lvo711717@gmail.com>
2024-02-29 11:29:35 +0200

    Create category controller and view

---
+++ LevchenkoVladWebApplication/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LevchenkoVladWebApplication.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
