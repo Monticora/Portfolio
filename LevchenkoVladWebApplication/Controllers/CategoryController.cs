a4402c29f01be5aff2c9c7d8926391dab1bbdc28
lvo711717 <lvo711717@gmail.com>
2024-02-29 12:04:53 +0200

    Show category on the page

--- LevchenkoVladWebApplication/Controllers/CategoryController.cs
+++ LevchenkoVladWebApplication/Controllers/CategoryController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LevchenkoVladWebApplication.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LevchenkoVladWebApplication.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly PortfolioDbContext _databaseContext;
+        public CategoryController(PortfolioDbContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            var categoryList = _databaseContext.CategoriesTable.ToList();
+            return View(categoryList);
         }
     }
 }
