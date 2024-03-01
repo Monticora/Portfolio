3d8c6d8e10bc014bc4b7767a0bd3b1be3ea187e3
lvo711717 <lvo711717@gmail.com>
2024-03-01 10:22:58 +0200

    Crud operation for categories

--- LevchenkoVladWebApplication/Controllers/CategoryController.cs
+++ LevchenkoVladWebApplication/Controllers/CategoryController.cs
@@ -38,5 +38,65 @@ namespace LevchenkoVladWebApplication.Controllers
             }
             return View();
         }
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            
+            Category? categorFromDB = _databaseContext.CategoriesTable.Where(obj => obj.Id == id).FirstOrDefault();
+
+            if(categorFromDB == null) 
+            {
+                return NotFound();
+            }
+            return View(categorFromDB);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            //Custom validation that check do the name of the category have digit or not
+            if (char.IsDigit(category.Name, 0))
+            {
+                ModelState.AddModelError("name", "The category name can't have digits!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _databaseContext.CategoriesTable.Update(category);
+                _databaseContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categorFromDB = _databaseContext.CategoriesTable.Where(obj => obj.Id == id).FirstOrDefault();
+
+            if (categorFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(categorFromDB);
+        }
+        [HttpPost]
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? category = _databaseContext.CategoriesTable.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _databaseContext.CategoriesTable.Remove(category);
+            _databaseContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
