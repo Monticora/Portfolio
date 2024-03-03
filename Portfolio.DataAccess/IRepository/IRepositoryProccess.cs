b36db066bb7d8e2a58f70a5da212a1ceabcacc22
lvo711717 <lvo711717@gmail.com>
2024-03-03 18:18:29 +0200

    creating  generic repositories

---
+++ Portfolio.DataAccess/IRepository/IRepositoryProccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.DataAccess.IRepository
+{
+    public interface IRepositoryProccess<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetFirstOrDefuoult(Expression<Func<T, bool>> filter);
+        void Add(T item);
+        void Delete(T item);
+    }
+}
---
+++ Portfolio.DataAccess/Repository/RepositoryProccess.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Portfolio.DataAccess.Data;
+using Portfolio.DataAccess.IRepository;
+using System.Linq.Expressions;
+
+namespace Portfolio.DataAccess.Repository
+{
+    public class RepositoryProccess<T> : IRepositoryProccess<T> where T : class
+    {
+        private readonly PortfolioDbContext _portfolioDbContext;
+        internal DbSet<T> dbSet;
+
+        public RepositoryProccess(PortfolioDbContext portfolioDbContext)
+        {
+            _portfolioDbContext = portfolioDbContext;
+            this.dbSet = _portfolioDbContext.Set<T>();
+        }
+        public void Add(T item)
+        {
+            dbSet.Add(item);
+        }
+
+        public void Delete(T item)
+        {
+            dbSet.Remove(item);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefuoult(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+    }
+}
