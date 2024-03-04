345fb78541beb2e82b0c62e936bf27c58cf3e891
lvo711717 <lvo711717@gmail.com>
2024-03-04 10:09:57 +0200

    ICategory and Category implementation

---
+++ Portfolio.DataAccess/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Portfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.DataAccess.IRepository
+{
+    public interface ICategoryRepository : IRepositoryProccess<Category>
+    {
+        void Update(Category category);
+        void Save();
+    }
+}
---
+++ Portfolio.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using Portfolio.DataAccess.Data;
+using Portfolio.DataAccess.IRepository;
+using Portfolio.Models;
+using System.Linq.Expressions;
+
+namespace Portfolio.DataAccess.Repository
+{
+    public class CategoryRepository : RepositoryProccess<Category>, ICategoryRepository
+    {
+        private PortfolioDbContext _portfolioDbContext;
+        public CategoryRepository(PortfolioDbContext portfolioDbContext) : base(portfolioDbContext)
+        {
+            _portfolioDbContext = portfolioDbContext;
+        }
+
+        public void Save()
+        {
+            _portfolioDbContext.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            _portfolioDbContext.CategoriesTable.Update(category);
+        }
+    }
+}
